bf12f1b5acc57b706c6f2f37fe951c8923062c04
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-18 22:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function calculatesFarePrice (number1,number2) {
      else if (feet > 400 && feet < 2000) {
        return ((feet-400)*0.02);
      }
-     else if (feet > 2000) {
+     else if (feet > 2000 && feet < 2500) {
        return 25;
      }
      else if (feet > 2500) {
