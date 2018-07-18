211e6bf1db440d1b977431312a3211aa0f81a8fe
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-18 22:40:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ function distanceTravelledInFeet (number1,number2) {
   }
 }
 
+let feetTravelled = distanceTravelledInFeet()
+
 function calculatesFarePrice (number1,number2) {
    if (number1 >= number2) {
    let feet = ((number1-number2) * 264)
